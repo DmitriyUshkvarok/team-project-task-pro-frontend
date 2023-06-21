86d0204c61af6fcd39c46348c90ff2df9ef0cfe9
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-06-21 14:08:51 +0300

    add handleLogout and add Notiflix

--- src/Components/Sidebar/LogOut/LogOut.jsx
+++ src/Components/Sidebar/LogOut/LogOut.jsx
@@ -1,9 +1,37 @@
+import { useDispatch } from 'react-redux';
+import Notiflix from 'notiflix';
+import authOperation from '../../../redux/auth/authOperation';
+
 import { Container, BtnLogOut } from './LogOut.styled';
 
 const LogOut = () => {
+  const dispatch = useDispatch();
+
+  const handleClickLogOut = () => {
+    Notiflix.Confirm.init({
+      width: '260px',
+      position: 'center-top',
+      fontFamily: 'Poppins',
+      titleColor: '#bedbb0',
+      okButtonBackground: '#bedbb0',
+    });
+    Notiflix.Confirm.show(
+      'Confirmation',
+      'Are you sure you want to log out?',
+      'Yes',
+      'No',
+      () => {
+        dispatch(authOperation.logOut());
+      },
+      () => {}
+    );
+  };
+
   return (
     <Container>
-      <BtnLogOut type="buttom">Log out</BtnLogOut>
+      <BtnLogOut onClick={handleClickLogOut} type="buttom">
+        Log out
+      </BtnLogOut>
     </Container>
   );
 };
