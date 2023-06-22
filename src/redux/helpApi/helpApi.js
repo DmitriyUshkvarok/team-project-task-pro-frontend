13b1c58dd2a869db818af9e0bd504bdbce6dbd5e
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-06-22 23:46:14 +0300

    helpApi

---
+++ src/redux/helpApi/helpApi.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const helpApi = createApi({
+  reducerPath: 'helpApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://task-pro-app.onrender.com/api/',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getHelp: builder.query({
+      query: () => 'help/',
+    }),
+    postComment: builder.mutation({
+      query: ({ comment, email }) => ({
+        url: 'help/',
+        method: 'POST',
+        body: { comment, email },
+      }),
+    }),
+  }),
+});
+
+export const { useGetHelpQuery, usePostCommentMutation } = helpApi;
