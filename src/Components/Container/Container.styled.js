8adf2c8d7de9d967cd0d11c0efa86941c66c7f1d
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-06-22 21:15:51 +0300

    add welcome page

--- src/Components/Container/Container.styled.js
+++ src/Components/Container/Container.styled.js
@@ -8,6 +8,7 @@ export const StyleContainer = styled.div`
 
   @media screen and (min-width: 768px) {
     width: 768px;
+    
   }
 
   @media screen and (min-width: 1280px) {
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,18 +1,37 @@
-import { StyleSectionWelcomePage, IconStyled } from './WelcomePage.styled';
-import { Link } from 'react-router-dom';
+/* eslint-disable react/no-unescaped-entities */
+import {
+  StyleSectionWelcomePage,
+  LogoBlock,
+  Logo,
+  Content,
+  Link,
+  ImgLogo,
+  ImgIcon
+} from './WelcomePage.styled';
 
-import imageUrl from '../../images/icons/iconsPng/imageLogo.png';
-import iconUrl from '../../images/icons/icons.svg';
+import imageUrl from '../../images/icons/iconsPng/menLaptopBig.png';
+import iconUrl from '../../images/icons/sprite/icons.svg';
 
 const WelcomePage = () => {
   return (
     <StyleSectionWelcomePage>
-      <img src={imageUrl} alt="Boy with laptop" width="162" height="162" />
-      <IconStyled>
-        <use href={`${iconUrl}#icon-iconLogo`}></use>
-      </IconStyled>
-      <Link to="/auth/login">Log In</Link>
-      <Link to="/auth/register">Register</Link>
+      <LogoBlock>
+        <ImgLogo src={imageUrl} alt="Boy with laptop" />
+        <Logo>
+          <ImgIcon>
+            <use href={`${iconUrl}#icon-iconLogo`}></use>
+          </ImgIcon>
+          Task Pro
+        </Logo>
+      </LogoBlock>
+      <Content>
+        Supercharge your productivity and take control of your tasks with Task
+        Pro - Don't wait, start achieving your goals now!
+      </Content>
+      <nav>
+        <Link to="/auth/register">Register</Link>
+        <Link to="/auth/login">Log In</Link>
+      </nav>
     </StyleSectionWelcomePage>
   );
 };
