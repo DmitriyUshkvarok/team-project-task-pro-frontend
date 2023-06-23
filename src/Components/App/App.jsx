0906e8c5e5ad04de859a6fbab41dcc5e6e16a1c2
OPaslavskiy <bankir.op@gmail.com>
2023-06-23 11:08:30 +0300

    radio buttons work

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -10,6 +10,7 @@ import authSelector from '../../redux/auth/authSelector';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
+import ModalEditCard from '../Modals/EditCard/EditCard';
 
 const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage'));
 const AuthPage = lazy(() => import('../../pages/AuthPage/AuthPage'));
@@ -31,6 +32,7 @@ function App() {
         ) : (
           <Suspense fallback={<p>Loading...</p>}>
             <Container>
+              <ModalEditCard />
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route
