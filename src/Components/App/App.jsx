8b2f1770352acdaaf25b5e5a14d8c95f0ac5aaf7
OPaslavskiy <bankir.op@gmail.com>
2023-06-23 22:30:53 +0300

    app deleted modaladdcard

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -31,7 +31,6 @@ function App() {
         ) : (
           <Suspense fallback={<p>Loading...</p>}>
             <Container>
-              <ModalAddCard />
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route
