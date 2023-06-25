f3d09d7540c50c22dc5abfad11947fe498df7b4b
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-06-26 01:25:54 +0300

    add header@sidebar style, fix some code

--- src/Components/Modals/Card/Card.styled.js
+++ src/Components/Modals/Card/Card.styled.js
@@ -1,13 +1,10 @@
 import styled from 'styled-components';
 import { ErrorMessage, Field } from 'formik';
 
-import { FaChevronDown } from 'react-icons/fa';
-
-
 export const AddCardModal = styled.div`
   width: 335px;
-  background-color: var(--blackColor);
-
+  height: 522px;
+  background-color: var(--whiteColor);
   border-radius: var(--borderRadius8);
   padding: 24px;
 
