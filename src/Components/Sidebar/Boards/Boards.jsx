5f38d8af65df11760227ed974e84ea9e424edf3d
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-06-26 11:11:20 +0300

    fix icons-test and wrote function findIcons in Board component

--- src/Components/Sidebar/Boards/Boards.jsx
+++ src/Components/Sidebar/Boards/Boards.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
-import { Link, redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import { useGetFetchBoardsQuery } from '../../../redux/boadrApi/boardApi';
+
 import url from '../../../images/icons/sprite/icons.svg';
+import icons from '../../icons.json';
 import {
   ListBoard,
   ItemBoard,
   WrapTitle,
-  IconProject,
   TitleBoard,
   WrapIcons,
   BtnIcon,
@@ -17,7 +18,14 @@ import {
 const Boards = () => {
   const [selectedItem, setSelectedItem] = useState(null);
 
-  const { data: boards, isLoading } = useGetFetchBoardsQuery();
+  const { data: boards, isLoading, error } = useGetFetchBoardsQuery();
+
+  const findIconsUser = (iconId) => {
+    const findIcon = icons.find((icon) => icon.id === iconId);
+    if (findIcon) {
+      return `${url}${findIcon.path}`;
+    }
+  };
 
   const handleItemClick = (index) => {
     setSelectedItem((prevSelectedItem) =>
@@ -27,14 +35,16 @@ const Boards = () => {
 
   return (
     <ListBoard>
-      {boards?.map(({ _id, title }, index) => (
+      {boards?.map(({ _id, title, iconId }, index) => (
         <Link to={`/${_id}/${title}`} key={_id}>
           <ItemBoard
             isSelected={selectedItem === index}
             onClick={() => handleItemClick(index)}
           >
             <WrapTitle>
-              <IconProject />
+              <IconStyled width="18" height="18">
+                <use xlinkHref={findIconsUser(iconId)} />
+              </IconStyled>
               <TitleBoard>{title}</TitleBoard>
             </WrapTitle>
 
--- src/Components/test-icon-board.jsx
+++ src/Components/test-icon-board.jsx
@@ -1,18 +1,17 @@
 import url from '../images/icons/sprite/icons.svg';
 
-
 const BoardComponentTest = () => {
   const { data, error, isLoading } = useGetBoardsQuery();
   const icons = [
     //   здесь id бордов из спрайта
-    { id: 1, path: '#icon-board-1' },
-    { id: 2, path: '#icon-board-2' },
-    { id: 3, path: '#icon-board-3' },
-    { id: 4, path: '#icon-board-4' },
-    { id: 5, path: '#icon-board-5' },
-    { id: 6, path: '#icon-board-6' },
-    { id: 7, path: '#icon-board-7' },
-    { id: 8, path: '#icon-board-8' },
+    { id: '1', path: '#icon-board-1' },
+    { id: '2', path: '#icon-board-2' },
+    { id: '3', path: '#icon-board-3' },
+    { id: '4', path: '#icon-board-4' },
+    { id: '5', path: '#icon-board-5' },
+    { id: '6', path: '#icon-board-6' },
+    { id: '7', path: '#icon-board-7' },
+    { id: '8', path: '#icon-board-8' },
   ];
 
   if (error) {
@@ -33,11 +32,9 @@ const BoardComponentTest = () => {
           if (selectedIcon) {
             const iconPath = `${url}${selectedIcon.path}`; /* здесь url содержит путь к спрайту */
             return (
-              <div key={board.id}>
-                <svg>
-                  <use xlinkHref={iconPath} />
-                </svg>
-              </div>
+              <svg>
+                <use xlinkHref={iconPath} />
+              </svg>
             );
           }
 
