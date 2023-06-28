000989747155f700891dc67489c99dad5416ca26
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-06-28 11:50:32 +0300

    width +  height

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -31,7 +31,7 @@ const Sidebar = ({ isSidebarOpen }) => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsSticky(window.innerWidth >= 1440);
+      setIsSticky(window.innerWidth >= 1280);
     };
 
     window.addEventListener('resize', handleResize);
