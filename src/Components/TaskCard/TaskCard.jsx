6c2d47a5ee364b972a9ce2f1a99074ddd359e35d
OPaslavskiy <bankir.op@gmail.com>
2023-06-28 07:50:37 +0300

    fix bug with components Tasks. All good

--- src/Components/TaskCard/TaskCard.jsx
+++ src/Components/TaskCard/TaskCard.jsx
@@ -18,11 +18,8 @@ import {
 } from './TaskCard.styled';
 import url from '../../images/icons/sprite/icons.svg';
 
-const TaskCard = ({ data, id }) => {
-  console.log(data);
+const TaskCard = ({ task }) => {
   const maxLength = 97;
-  // const longText = `Conduct in-depth research and analysis on the project's topic, gather relevant data, and identify key insights to inform decision-making and project planning.`;
-  // const color = '#e09cb5';
 
   function colorSwitch(priority) {
     let color;
@@ -48,70 +45,59 @@ const TaskCard = ({ data, id }) => {
   }
 
   return (
-    <CardBg>
-      {data.tasks
-        .filter((task) => id === task.column)
-        .map((task) => {
-          return (
-            <Card
-              key={task._id}
-              style={{ backgroundColor: `${colorSwitch(task.priority)}` }}
-            >
-              <CardTitle>{task.title}</CardTitle>
+    <CardBg style={{ backgroundColor: `${colorSwitch(task.priority)}` }}>
+      <Card key={task._id}>
+        <CardTitle>{task.title}</CardTitle>
 
-              <CardDescription>
-                <CardText>
-                  {task.description.length >= maxLength
-                    ? `${task.description.substring(0, maxLength)}...`
-                    : task.description}
-                </CardText>
-              </CardDescription>
+        <CardDescription>
+          <CardText>
+            {task.description.length >= maxLength
+              ? `${task.description.substring(0, maxLength)}...`
+              : task.description}
+          </CardText>
+        </CardDescription>
 
-              <CardBottom>
-                <CardBottomGrop>
-                  <CardPriority>
-                    <CardSubtitle>Priority</CardSubtitle>
-                    <Circle>
-                      <div
-                        style={{
-                          backgroundColor: `${colorSwitch(task.priority)}`,
-                          height: '12px',
-                          width: '12px',
-                          borderRadius: '50%',
-                        }}
-                      ></div>
-                      <Priority>{task.priority}</Priority>
-                    </Circle>
-                  </CardPriority>
-                  <CardDeadline>
-                    <CardSubtitle>Deadline</CardSubtitle>
-                    <Deadline>{task.deadline}</Deadline>
-                  </CardDeadline>
-                </CardBottomGrop>
+        <CardBottom>
+          <CardBottomGrop>
+            <CardPriority>
+              <CardSubtitle>Priority</CardSubtitle>
+              <Circle>
+                <div
+                  style={{
+                    backgroundColor: `${colorSwitch(task.priority)}`,
+                    height: '12px',
+                    width: '12px',
+                    borderRadius: '50%',
+                  }}
+                ></div>
+                <Priority>{task.priority}</Priority>
+              </Circle>
+            </CardPriority>
+            <CardDeadline>
+              <CardSubtitle>Deadline</CardSubtitle>
+              <Deadline>{task.deadline}</Deadline>
+            </CardDeadline>
+          </CardBottomGrop>
 
-                <CardBtnGrope>
-                  <CardBtn type="button">
-                    <CardIcon>
-                      <use
-                        xlinkHref={`${url}#icon-arrow-circle-broken-right`}
-                      />
-                    </CardIcon>
-                  </CardBtn>
-                  <CardBtn type="button">
-                    <CardIcon stroke="grey">
-                      <use xlinkHref={`${url}#icon-pencil-01`} />
-                    </CardIcon>
-                  </CardBtn>
-                  <CardBtn type="button">
-                    <CardIcon>
-                      <use xlinkHref={`${url}#icon-trash-04`} />
-                    </CardIcon>
-                  </CardBtn>
-                </CardBtnGrope>
-              </CardBottom>
-            </Card>
-          );
-        })}
+          <CardBtnGrope>
+            <CardBtn type="button">
+              <CardIcon>
+                <use xlinkHref={`${url}#icon-arrow-circle-broken-right`} />
+              </CardIcon>
+            </CardBtn>
+            <CardBtn type="button">
+              <CardIcon stroke="grey">
+                <use xlinkHref={`${url}#icon-pencil-01`} />
+              </CardIcon>
+            </CardBtn>
+            <CardBtn type="button">
+              <CardIcon>
+                <use xlinkHref={`${url}#icon-trash-04`} />
+              </CardIcon>
+            </CardBtn>
+          </CardBtnGrope>
+        </CardBottom>
+      </Card>
     </CardBg>
   );
 };
