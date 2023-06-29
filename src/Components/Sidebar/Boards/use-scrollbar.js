fece966d5d4111a8dcf02914b2b745d0ff8ec846
Evgen Zaharevich <metrovyy@gmail.com>
2023-06-29 17:08:05 +0300

    Create custom scrollbar

---
+++ src/Components/Sidebar/Boards/use-scrollbar.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { OverlayScrollbars } from 'overlayscrollbars';
+
+const config = {
+  scrollbars: {
+    visibility: 'auto',
+    autoHide: 'leave',
+  },
+};
+
+const useScrollbar = (root, hasScroll) => {
+  useEffect(() => {
+    let scrollbars;
+    if ((root.current, hasScroll)) {
+      scrollbars = OverlayScrollbars(root.current, config);
+    }
+
+    return () => {
+      if (scrollbars) {
+        scrollbars.destroy();
+      }
+    };
+  }, [root, hasScroll]);
+};
+
+export { useScrollbar };
