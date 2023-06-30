01e12e1567d7de15cad4e2df058daebc870a074b
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-06-30 23:38:21 +0300

    fix loader refresh ,added loader for button add task

--- src/Components/Modals/ButtonModal/ButtonModal.jsx
+++ src/Components/Modals/ButtonModal/ButtonModal.jsx
@@ -1,16 +1,23 @@
 import url from '../../../images/icons/sprite/icons.svg';
 import { Button, ContainerIconButton } from './ButtonModal.styled';
+import { LoaderForDeleted } from '../../Loader/LoaderDeleted/LoaderDeleted';
 
-const ButtonModal = ({ buttonName }) => {
+const ButtonModal = ({ buttonName, isLoading }) => {
   return (
     <>
-      <Button type="submit">
-        <ContainerIconButton>
-          <svg width="14" height="14">
-            <use xlinkHref={`${url}#icon-plus`} />
-          </svg>
-        </ContainerIconButton>
-        {buttonName}
+      <Button type="submit" disabled={isLoading}>
+        {isLoading ? (
+          <LoaderForDeleted />
+        ) : (
+          <>
+            <ContainerIconButton>
+              <svg width="14" height="14">
+                <use xlinkHref={`${url}#icon-plus`} />
+              </svg>
+            </ContainerIconButton>
+            {buttonName}
+          </>
+        )}
       </Button>
     </>
   );
