0dbffcb74791a493148ba5c855575c8048f4b02a
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-07-01 16:52:33 +0300

    add pointer logo

--- src/Components/Sidebar/Logo/Logo.jsx
+++ src/Components/Sidebar/Logo/Logo.jsx
@@ -1,17 +1,19 @@
-import { Wrap, ContainerIcon, LogoText, IconStyled } from './Logo.styled';
+import { Wrap, ContainerIcon, LogoText, IconStyled, LinkBox } from './Logo.styled';
 
 import url from '../../../images/icons/sprite/icons.svg';
 
 const Logo = () => {
+
   return (
     <Wrap>
-      <ContainerIcon>
-        <IconStyled width="16" height="16">
-          <use xlinkHref={`${url}#icon-Vector-1`} />
-        </IconStyled>
-      </ContainerIcon>
-
-      <LogoText href="#">Task Pro</LogoText>
+      <LinkBox  to="/">
+        <ContainerIcon>
+          <IconStyled width="16" height="16">
+            <use xlinkHref={`${url}#icon-Vector-1`} />
+          </IconStyled>
+        </ContainerIcon>
+        <LogoText>Task Pro</LogoText>
+      </LinkBox>
     </Wrap>
   );
 };
--- src/Components/Sidebar/Logo/Logo.styled.js
+++ src/Components/Sidebar/Logo/Logo.styled.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Wrap = styled.div`
   display: flex;
   padding-left: 14px;
-  align-items: center;
-  gap: 8px;
-
+  
   @media screen and (min-width: 1280px) {
     padding-left: 24px;
   }
+`;
+
+export const LinkBox = styled(Link)`
+  display: flex;
+ 
+  align-items: center;
+  gap: 8px;
+
+ 
 `;
 
 export const ContainerIcon = styled.span`
@@ -25,7 +33,7 @@ export const IconStyled = styled.svg`
   fill: var(--logoIconColor);
 `;
 
-export const LogoText = styled.a`
+export const LogoText = styled.h1`
   font-family: var(--poppinsFont);
 
   font-weight: var(--fontWeight600);
