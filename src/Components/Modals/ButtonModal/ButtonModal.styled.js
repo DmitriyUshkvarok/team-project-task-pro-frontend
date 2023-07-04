d64becbc5f215dc53f3eb00509e3839dc60d0ba2
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-07-04 23:16:06 +0300

    add styled to board btn

--- src/Components/Modals/ButtonModal/ButtonModal.styled.js
+++ src/Components/Modals/ButtonModal/ButtonModal.styled.js
@@ -1,4 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+
+const pulseAnimation = keyframes`
+  0% {
+    transform: scale(1.03);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.03);
+  }
+`;
 
 export const Button = styled.button`
   display: flex;
@@ -22,6 +35,50 @@ export const Button = styled.button`
   font-weight: var(--fontWeight500);
   letter-spacing: -0.28px;
   cursor: pointer;
+
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  transition: all 0.3s;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 200%;
+    height: 100%;
+    background: linear-gradient(
+      45deg,
+      rgba(255, 255, 255, 0.8),
+      rgba(255, 255, 255, 0)
+    );
+    top: 0;
+    left: -100%;
+    transform: rotate(45deg);
+    transition: all 0.3s;
+    opacity: 0;
+  }
+
+  &:hover,
+  &:focus {
+    transform: translateY(-2px);
+    box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.2);
+    animation: ${pulseAnimation} 1s ease-in-out infinite;
+  }
+
+  &:hover:before,
+  &:focus:before {
+    left: 100%;
+    opacity: 1;
+    transition: left 2s ease-in-out, opacity 0.3s ease-in-out;
+  }
+
+  &:active {
+    transform: translateY(1px);
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
+
 `;
 
 export const ContainerIconButton = styled.span`
